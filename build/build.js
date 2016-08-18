7d8058375047f23ce916507c2c336958bc2a4712
hiendv <neo.go.vn@gmail.com>
2016-08-18 23:50:24 +0700

    New build script

---
+++ build/build.js
@@ -0,0 +1,26 @@
+var pkg = require('../package.json')
+var path = require('path')
+var fs = require('fs')
+var archiver = require('archiver')
+
+var name = path.join(__dirname, 'v' + pkg.version + '.zip')
+var output = fs.createWriteStream(name)
+var archive = archiver('zip')
+
+output.on('close', function() {
+  console.log(name + ' is created')
+})
+
+archive.on('error', function(err) {
+  throw err
+})
+
+archive.pipe(output)
+
+archive
+  .directory(path.join(__dirname, '../lib/'), 'lib')
+  .directory(path.join(__dirname, '../public/'), 'public')
+  .file(path.join(__dirname, '../package.json'), {name: 'package.json'})
+  .file(path.join(__dirname, '../.env.example'), {name: '.env'})
+  .file(path.join(__dirname, '../README.md'), {name: 'README.md'})
+  .finalize()
