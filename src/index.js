9926d7d71256103b303c4f005fe7ec597c68b625
hiendv <neo.go.vn@gmail.com>
2016-08-19 19:34:33 +0700

    fix #1

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 require('dotenv').config()
 
 import {version} from '../package.json'
-import path from 'path'
 import crypto from 'crypto'
 
 import Koa from 'koa'
 import Route from 'koa-route'
-import Serve from 'koa-static'
+import Send from 'koa-send'
 
 import Octokat from 'octokat'
 import Badge from './Badge'
@@ -19,11 +18,6 @@ app.context.github = new Octokat({
 
 const badge = new Badge(app.context, process.env)
 
-app.use(Serve(path.join(__dirname, '../public'), {
-  gzip: true,
-  maxage: 0
-}))
-
 app.use(Route.get('/', function * () {
   this.body = 'Hireable v' + version
 }))
@@ -33,13 +27,13 @@ app.use(Route.get('/p/:user', function * (user) {
 }))
 
 app.use(Route.get('/:user/:repo?', function * show (id, repo) {
+  let source
   yield badge.show(id, repo).then(src => {
-    this.body = null
-    this.response.etag = crypto.createHash('md5').update(src).digest('hex')
-    this.set('Cache-Control', 'no-cache, no-store, must-revalidate')
-    this.redirect(src)
-    this.status = 301
+    this.set('etag', crypto.createHash('md5').update(src).digest('hex'))
+    this.set('Cache-Control', 'private')
+    source = src
   })
+  yield Send(this, './public/' + source)
 }))
 
 app.listen(process.env.APP_PORT)
