64094903b587cbaa4f24a46d342224a5630c70f2
hiendv <neo.go.vn@gmail.com>
2016-08-26 17:11:40 +0700

    tests added

--- test/index.js
+++ test/index.js
@@ -3,10 +3,44 @@ import assert from 'assert'
 
 import '../src/index.js'
 
+let url = function (route) {
+  return 'http://127.0.0.1:' + process.env.APP_PORT + route;
+}
+
 describe('GET /', () => {
   it('should return 200', done => {
-    http.get('http://127.0.0.1:' + process.env.APP_PORT, res => {
+    http.get(url('/'), res => {
       assert.equal(200, res.statusCode)
+      done()
+    })
+  })
+})
+
+describe('GET /ghost', () => {
+  it('should return 200', done => {
+    http.get(url('/ghost'), res => {
+      assert.equal(200, res.statusCode)
+      done()
+    })
+  })
+
+  it('should have headers', done => {
+    http.get(url('/ghost'), res => {
+      assert(res.headers)
+      done()
+    })
+  })
+
+  it('should have content type header', done => {
+    http.get(url('/ghost'), res => {
+      assert(res.headers['content-type'])
+      done()
+    })
+  })
+
+  it('should return an image', done => {
+    http.get(url('/ghost'), res => {
+      assert(res.headers['content-type'].indexOf('image/') >= 0)
       done()
     })
   })
