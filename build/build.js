cfe7ed4b911ed061760fcba0f6d57fbd0fa61b78
hiendv <neo.go.vn@gmail.com>
2016-08-26 16:45:36 +0700

    Update build script: Assets moving with command

--- build/build.js
+++ build/build.js
@@ -19,7 +19,6 @@ archive.pipe(output)
 
 archive
   .directory(path.join(__dirname, '../lib/'), 'lib')
-  .directory(path.join(__dirname, '../src/assets/'), 'lib/assets')
   .file(path.join(__dirname, '../package.json'), {name: 'package.json'})
   .file(path.join(__dirname, '../.env.example'), {name: '.env'})
   .file(path.join(__dirname, '../README.md'), {name: 'README.md'})
