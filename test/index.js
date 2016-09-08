bbe6d7193c409821680700c30f651c2056d83eb2
hiendv <neo.go.vn@gmail.com>
2016-09-09 00:34:52 +0700

    Update & add tests

--- test/index.js
+++ test/index.js
@@ -3,13 +3,21 @@
 import http from 'http'
 import assert from 'assert'
 
-import '../src/index.js'
+import App from '../src/index.js'
 
 let url = function (route) {
   return 'http://127.0.0.1:' + process.env.APP_PORT + route
 }
 
 describe('GET /', () => {
+  before(function () {
+    App.listen(process.env.APP_PORT)
+  })
+
+  after(function () {
+    App.close()
+  })
+
   it('should return 200', done => {
     http.get(url('/'), res => {
       assert.equal(200, res.statusCode)
@@ -19,6 +27,14 @@ describe('GET /', () => {
 })
 
 describe('GET /ghost', () => {
+  before(function () {
+    App.listen(process.env.APP_PORT)
+  })
+
+  after(function () {
+    App.close()
+  })
+
   it('should return 200', done => {
     http.get(url('/ghost'), res => {
       assert.equal(200, res.statusCode)
@@ -43,6 +59,13 @@ describe('GET /ghost', () => {
   it('should return an image', done => {
     http.get(url('/ghost'), res => {
       assert(res.headers['content-type'].indexOf('image/') >= 0)
+      done()
+    })
+  })
+
+  it('should return no', done => {
+    http.get(url('/ghost'), res => {
+      assert(res.headers['hireable'] === '0')
       done()
     })
   })
