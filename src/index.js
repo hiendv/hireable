40412cc29e1d3fab5d6cd1015319bc6df6e43ef8
hiendv <neo.go.vn@gmail.com>
2016-09-08 13:15:10 +0700

    Remove  & add

--- src/index.js
+++ src/index.js
@@ -1,40 +1,70 @@
 require('dotenv').config()
 
+import http from 'http'
+import url from 'url'
+
+import Pattern from 'url-pattern'
+import Send from 'send'
+
 import {version} from '../package.json'
-import crypto from 'crypto'
-
-import Koa from 'koa'
-import Route from 'koa-route'
-import Send from 'koa-send'
-
 import Badge from './Badge'
 
-const app = new Koa()
-const badge = new Badge()
+const routes = {
+  user () {
+    return {
+      pattern: new Pattern('/:user'),
+      then (request, response, params) {
+        (new Badge()).show(params.user).then(user => {
+          // ETag is automatically generated
+          response.setHeader('Cache-Control', 'private')
+          Send(request, user.badge).pipe(response)
+        })
+      }
+    }
+  },
+  profile () {
+    return {
+      pattern: new Pattern('/p/:user'),
+      then (request, response, params) {
+        response.writeHead(302, {
+          'Location': 'https://github.com/' + params.user
+        })
+        response.end()
+      }
+    }
+  }
+}
 
-app.use(Route.get('/', function * () {
-  this.body = 'Hireable v' + version
-}))
+const app = http.createServer(function (request, response) {
+  if (request.method !== 'GET') {
+    // Only GET
+    response.writeHead(403)
+    response.end()
+    return
+  }
 
-app.use(Route.get('/p/:user', function * (user) {
-  this.redirect('https://github.com/' + user)
-}))
+  let parts = url.parse(request.url)
 
-app.use(Route.get('/:user', function * show (username) {
-  let source
+  if (parts.pathname === '/favicon.ico') {
+    response.writeHead(404)
+    response.end()
+  }
 
-  yield badge.show(username).then(user => {
-    this.set('ETag', crypto.createHash('md5').update(JSON.stringify(user)).digest('hex'))
-    this.set('Cache-Control', 'private')
-    source = user.badge
-  })
+  for (let key in routes) {
+    let route = routes[key]()
+    let params = route.pattern.match(parts.pathname)
+    if (!params) {
+      continue
+    }
+    route.then.call(this, request, response, params)
+    return
+  }
 
-  yield Send(this, source, {
-    root: '/'
-  })
-}))
+  response.writeHead(200)
+  response.end('Hireable v' + version)
+})
 
-app.listen(process.env.APP_PORT)
-
-console.log('Listening on :' + process.env.APP_PORT)
-console.log('Visit http://localhost:' + process.env.APP_PORT)
+app.listen(process.env.APP_PORT, () => {
+  console.log('Listening on :' + process.env.APP_PORT)
+  console.log('Visit http://localhost:' + process.env.APP_PORT)
+})
