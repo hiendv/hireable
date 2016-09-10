cabffc33ce54d0720d4a2947045a7fb1b755fd1f
hiendv <neo.go.vn@gmail.com>
2016-09-10 15:11:35 +0700

    slashesDenoteHost

--- src/index.js
+++ src/index.js
@@ -44,7 +44,7 @@ const app = http.createServer(function (request, response) {
     return
   }
 
-  let parts = url.parse(request.url)
+  let parts = url.parse(request.url, false, true)
 
   if (parts.pathname === '/favicon.ico') {
     response.writeHead(404)
